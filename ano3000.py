eef4b46e2e7e71b42e355cde82e3ea29530b6585
Anne Caroline <91013975+annecaroline00@users.noreply.github.com>
2022-03-28 01:15:40 -0300

    Add files via upload

---
+++ ano3000.py
@@ -0,0 +1,10 @@
+print("em que ano você nasceu?")
+nascimento = input()
+nascimento = int(nascimento)
+ 
+print("para qual ano você quer saber a sua idade?")
+ano_escolhido = input()
+ano_escolhido = int(ano_escolhido)
+ 
+
+print(ano_escolhido - nascimento, "anos!")
---
+++ idade_anos_cachorros.py
@@ -0,0 +1,7 @@
+print("quantos você tem?")
+idade = input()
+idade = int(idade)*7
+ 
+ 
+
+print("se você fosse um cachorro, você teria", idade, "anos! :(")
