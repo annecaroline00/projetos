c6381173e9838272c1da09cd9800931bb537b013
annecaroline00 <annecaroline@ufpi.edu.br>
2022-04-18 00:39:19 -0300

    commit main

---
+++ QUIZ/questaodetempo.py
@@ -0,0 +1,12 @@
+print("Complete a música: 'I i i i follow...")
+
+respostas = input()
+
+if respostas == "you dark doom honey, I follow you":
+    print("I, I follow" 
+"\nI follow you Deep sea baby" 
+"\nI follow you" 
+"\nI, I follow, I follow you" 
+"\nDark doom honey, I follow you")
+else:
+    print("you are wrong")
