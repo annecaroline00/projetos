e006690c2c5482997debd0b33ff6272a3539dd94
annecaroline00 <annecaroline@ufpi.edu.br>
2022-04-17 22:42:42 -0300

    commit main

---
+++ SEM06/Sem-06-T1-Q3.py
@@ -0,0 +1,10 @@
+sinalx = input().strip()
+sinal = sinalx.upper()
+if sinal == "V" or sinal == "verde":
+    print("Siga")
+elif sinal == "A" or sinal == "amarelo":
+    print("Atenção")  
+else:
+    sinal == "E" or sinal == "vermelho"
+    print("Pare")
+
