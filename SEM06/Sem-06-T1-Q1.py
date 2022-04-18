21909daed419ed9a1fb596f89addb0fb66ab13ca
annecaroline00 <annecaroline@ufpi.edu.br>
2022-04-17 22:01:27 -0300

    commit main

---
+++ SEM06/Sem-06-T1-Q1.py
@@ -0,0 +1,7 @@
+nome = str(input().strip())
+sexo = input().strip()
+if sexo == "1" and "masculino":
+    print("Ilmo Sr.", nome)
+else:
+    sexo == "2" and "feminino"
+    print("Ilma Sra.", nome)
