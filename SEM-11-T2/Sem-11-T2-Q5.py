ae7c4619ef2ff38bf2e957fbbfe8c3ac06c019aa
annecaroline00 <annecaroline@ufpi.edu.br>
2022-05-24 21:50:27 -0300

    commit main

---
+++ SEM-11-T2/Sem-11-T2-Q5.py
@@ -0,0 +1,30 @@
+from tkinter import Y
+
+
+def primo(numb):
+    count = 0 
+    for i in range (1, numb + 1):
+        if numb % 1 == 0:
+            count += 1
+        
+    if count == 2:
+        return print(numb)
+
+
+def main():
+    x = int(input())
+    z = int(input())
+
+    if (x > z):
+        y = x
+        x = z
+        z = y
+    
+    for w in range(x, z + 1):
+        primo(w)
+
+
+if __name__ == "__main__":
+    main()
+
+    
