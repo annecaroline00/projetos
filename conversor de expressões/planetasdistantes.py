0491165b1098cf11edd7ffba6f7eb39c4638cd21
annecaroline00 <annecaroline@ufpi.edu.br>
2022-06-26 23:37:59 -0300

    commit main

--- conversor de expressões/planetasdistantes.py
+++ conversor de expressões/planetasdistantes.py
@@ -1,30 +1,11 @@
-def displayMenu():
-    print('tradutor de expressões')
-    print('='*13)
-    print('Menu: ')
-    print('     Mercúrio')
-    print('     Plutão')
-    print('     Jupiter')
+
+planetas = {}
+
+while True:
+    nome = input().strip()
+    distancia = float(input())
+    planetas= [nome, distancia]
 
 
-
-textSpeakDictionary = {
-    'Mercurio' : '91700000',
-    'Plutão' : '5 bilhões',
-    'Jupiter' : '628 milhões',
-
-}
- 
-
-running = True
-
-displayMenu()
-ordem = input('Digite um planeta: ')
-while running == True:
-
-    menuChoice = input('>_').lower()
-
-    if menuChoice == 'Mercurio':
-        print('Mercurio está a 91700000km da Terra')
-
+    print(planetas)
     
